9ed90d38fcbfac140b63bfca837ea0aad59f5cb8
Cauan-Leandro <165155389+Cauan-Leandro@users.noreply.github.com>
2024-03-27 11:31:05 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,16 @@
-<<<<<<< HEAD
-alert("bem vindo ao jogo do numero secreto");
+let numeroSecreto = Math.floor(Math.random() * 5);
+let numeroTentativa = 3;
 
-let numeroSecreto = 3;
-let numeroDigitado = parseInt(prompt("digite um numero de 1 a 5"));
-
-if(numeroDigitado === numeroSecreto){
-    alert('Parabéns voce acertou o numero secreto! :)');
-}else if(numeroDigitado > numeroSecreto){
-    alert ('Voce digitou um numero maior que o numero secreto');
-} else {
-    alert ('Voce digidou um numero menor que o numero secreto!');
+while (numeroTentativa > 0) {
+    let numeroDigitado = parseInt(prompt("digite um numero de 1 a 5"));
+    if (numeroDigitado === numeroSecreto){
+        alert("Parabéns voce acertou!");
+        break;
+    } else if (numeroDigitado > numeroSecreto){
+        alert('Voce digitou um numero maior que o numero secreto!');
+    } else {
+        alert('Voce digidou um numero menor que o numero secreto!');
+    }
+     alert(`voce ainda tem ${numeroTentativa} tentativas`);
+     numeroTentativa = numeroTentativa - 1;
 }
-
-
-=======
-alert('Bem vindo ao jogo do número secreto');
-
-let numeroSecreto = 3;
-let numeroDigitado = parseInt(prompt('Digite um número de 1 a 5'));
-
-if(numeroDigitado === numeroSecreto){
-    alert('Parabéns, você acertou!');
-} else if(numeroDigitado > numeroSecreto){
-    alert('Você digitou um número maior que o número secreto');
-} else {
-    alert('Você digitou um número menor que o número secreto');
-}
->>>>>>> cf6240998e1358a50cf71636d7744469164cfa22
