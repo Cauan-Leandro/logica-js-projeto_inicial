e5e2443881fc3801f2f0822578ea228822fd5c25
Cauan-Leandro <165155389+Cauan-Leandro@users.noreply.github.com>
2024-03-28 10:49:13 -0300

    Create operação soma.js

---
+++ operação soma.js
@@ -0,0 +1,6 @@
+//crie um programa que o usuario entra com dois valores e o sistema soma e imprime em um alert
+let primeiro = parseInt (prompt (`Digite o primeiro numero para a soma`));
+let segundo = parseInt (prompt (`Digite o segundo numero para a soma`));
+//soma dos valores
+let soma = (primeiro + segundo);
+alert (`Seu resultado é ${soma}`);
