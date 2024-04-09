91df111ae73efa5c39d07f23cbb58bc09d58f1b3
Cauan-Leandro <165155389+Cauan-Leandro@users.noreply.github.com>
2024-04-09 07:54:01 -0300

    Create tabela.js

---
+++ tabela.js
@@ -0,0 +1,11 @@
+let atividades = [
+    ['acordar 6']
+    ['tomar cafê 7']
+    ['curso 8']
+];
+
+for(let i = 0; i < 2; i++){
+   for(let j = 0; j < 2; j++){
+        alert(atividades[i, i])
+    }
+}
