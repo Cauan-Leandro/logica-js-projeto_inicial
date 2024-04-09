0a15f2e33a6c52a3a78fb379b8c20ff5030bed13
Cauan-Leandro <165155389+Cauan-Leandro@users.noreply.github.com>
2024-04-09 07:53:32 -0300

    Delete dados.js

--- dados.js
+++
@@ -1,5 +0,0 @@
-//crie um programa que declare duas variaveis para nome e idade que apareça um alerta com o nome e a idade do usuario
-let nomeDigitado = prompt (`Digite seu nome`);
-let idadeDigitada = prompt (`Digite sua idade`);
-    alert (`Seu nome é ${nomeDigitado} e sua idade é ${idadeDigitada}`);
-alert (`Paranéns voce esta cadastrado no nosso programa de ensino :)`);
