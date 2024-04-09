8b6cc4d4a7c6bf9e4c055418a3f415e64d0827ce
Cauan-Leandro <165155389+Cauan-Leandro@users.noreply.github.com>
2024-04-09 10:27:04 -0300

    Create invert.js

---
+++ invert.js
@@ -0,0 +1,16 @@
+function inverterString() {
+
+    let inputString = prompt("Digite uma string para inverter:");
+
+    if (inputString === null || inputString === "") {
+        alert("Nenhuma string foi inserida.");
+        return;
+    }
+
+ 
+    let invertedString = inputString.split("").reverse().join("");
+
+    alert("String invertida: " + invertedString);
+}
+
+inverterString();
