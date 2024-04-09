9654fcd55170a94e9e716822ddd302b52aa4df72
Cauan-Leandro <165155389+Cauan-Leandro@users.noreply.github.com>
2024-04-09 07:53:30 -0300

    Create clicou.js

---
+++ clicou.js
@@ -0,0 +1,10 @@
+let contador = 0;
+function clicou() {
+    contador++;
+    alert (`Voce clicou ${contador} vezes`)
+}
+//crie uma funçao que ao clicar conta quantas vezes o usuario clicou.
+
+function zerarContador() {
+    contador = 0;
+}
